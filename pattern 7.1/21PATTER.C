b10405afcf893c58bb69fec7bd43cdb338d622d9
nirmalbhuva <114919399+nirmalbhuva@users.noreply.github.com>
2022-12-09 11:12:31 +0530

    pattern 7.1

---
+++ pattern 7.1/21PATTER.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,j;
+	char k;
+	clrscr();
+	for(i=1; i<=5 ;i++)
+	{
+	k='A';
+		for(j=1; j<=i; j++){
+			printf("%c",k++);
+		}
+		printf("\n");
+
+	}
+
+	getch();
+}
